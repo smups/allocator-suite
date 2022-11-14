ef0c1c5e9270cc0ffd3e6c08dac17d8d99953467
Raúl Wolters <56522208+smups@users.noreply.github.com>
2022-11-14 12:09:45 +0100

    removed unused const_fn feature and deprecated llvm-asm feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 #![feature(core_intrinsics)]
 #![feature(libstd_sys_internals)]
 #![feature(thread_local)]
-#![feature(const_fn)]
-#![feature(llvm_asm)]
 #![feature(nonnull_slice_from_raw_parts)]
 
 /// Path prediction macros for likely/unlikely intrinsics
